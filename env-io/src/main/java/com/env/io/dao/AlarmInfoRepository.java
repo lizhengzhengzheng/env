ba9a3f27bc540739df4c01167c0a9cd36c7e1dfa
lizhengzhengzheng <1044153973@qq.com>
2020-05-05 17:23:12 +0800

    X

--- env-io/src/main/java/com/env/io/dao/AlarmInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.env.io.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.env.io.entity.AlarmInfo;
-
-public interface AlarmInfoRepository extends JpaRepository<AlarmInfo,Integer> {
-
-}
--- env-io/src/main/java/com/env/io/dao/MonitoringDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.env.io.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.env.io.entity.MonitoringData;
-
-public interface MonitoringDataRepository extends JpaRepository<MonitoringData,Integer>{
-
-}
